33fc45218f80b39b2a0f06d2cd0a5c6cea786315
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-07-06 14:42:16 +0800

    Install mongoose

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 // import env variables
-require("dotenv").config();
+if(process.env.NODE_ENV != 'production') { 
+    require("dotenv").config(); 
+}
 // import express and create app
 const express = require("express");
 const app = express()
