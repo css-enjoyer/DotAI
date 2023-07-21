eef9c20741f8d2740541bda95a81b452f7737975
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-07-21 16:43:44 +0800

    Add links to hero pages for each hero image

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,6 +1,16 @@
 function Hero({image, name, className}) {
+
+	const openHeroPage = () => {
+		// Encode the hero name for the URL
+		const encodedName = encodeURIComponent(name);
+		// Generate the URL with the hero name
+		const url = `https://www.dota2.com/hero/${encodedName}`;
+		// Open the URL in a new tab
+		window.open(url, '_blank');
+	}
+
   	return (
-    	<div className={`Hero ${className}`}>
+    	<div className={`Hero ${className}`} onClick={openHeroPage}>
             <img src={image} alt={name} id="hero-img"></img>
             <p>{name}</p>
     	</div>
