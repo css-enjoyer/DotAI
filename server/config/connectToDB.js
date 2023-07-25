1cc1916cc3d2230da787c62df7a9f656cec876b6
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-07-25 14:05:47 +0800

    Clean up comments in connectToDB

--- server/config/connectToDB.js
+++ server/config/connectToDB.js
@@ -9,8 +9,6 @@ const mongoose = require("mongoose");
 const {Hero, createHeroModels} = require("../models/hero");
 const categorizeHeroes = require("../config/categorizeHeroes");
 // const uncategorizeHeroes = require("../config/uncategorizeHeroes");
-// const removeDuplicates = require("../models/removeDuplicates");
-// const removeHeroes = require("../models/removeHeroes");
 
 async function connectToDB() {
     try {
@@ -18,9 +16,7 @@ async function connectToDB() {
         console.log("Connected to MongoDB");
     
         // Create hero models after successful database connection
-        // removeDuplicates();
         // uncategorizeHeroes();
-        // await removeHeroes();
         await createHeroModels();
         categorizeHeroes();
     } catch (err) {
