0ee9d33afaf4c74616669013786c75605de712fb
Isaac Reyes <isaacjdjreyes@gmail.com>
2023-09-01 13:12:48 +0800

    Add noreferrer to contact anchors

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -7,10 +7,10 @@ function Contact() {
                 <h1>Contact</h1>
             </div>
             <div className="body">
-                <a href="https://github.com/css-enjoyer/DotAI" target="_blank">Project GitHub</a>
-                <a>isaacjdjreyes@gmail.com</a>
-                <a href="https://css-enjoyer.github.io/portfolio/" target="_blank">My Portfolio</a>
-                <a href="https://github.com/css-enjoyer" target="_blank">My GitHub</a>
+                <a href="https://github.com/css-enjoyer/DotAI" target="_blank" rel="noreferrer">Project GitHub</a>
+                <p>isaacjdjreyes@gmail.com</p>
+                <a href="https://css-enjoyer.github.io/portfolio/" target="_blank" rel="noreferrer">My Portfolio</a>
+                <a href="https://github.com/css-enjoyer" target="_blank" rel="noreferrer">My GitHub</a>
             </div>
         </div>
     );
