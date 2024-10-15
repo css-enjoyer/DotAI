e69818471640fe90486286316ee55fe7608d5635
Isaac <isaacjdjreyes@gmail.com>
2024-10-15 10:48:17 +0800

    Add last updated line

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,11 +7,15 @@ function About() {
                 <h1>About</h1>
             </div>
             <div className="body">
-                <p>DotAI is a project I made to simplify hero selection during crucial drafting phases. The built-in Dota 2 hero categorization often doesn't do the job effectively. DotAI acts as a smart tool for sorting heroes based on what's good for your team, and what's bad for the enemy, making hero selection smarter and smoother.
+                <p>DotAI is a side project I made to simplify hero selection during crucial drafting phases. The built-in Dota 2 hero categorization often doesn't do the job effectively. DotAI acts as a tool for sorting heroes based on what's good for your team, and what's bad for the enemy, making hero selection smarter , faster and easier.
                     <br/>
                     <br/>
-                I want to note that the categorizations you see here are entirely my own work, that these do not come from a professional background. If you have suggestions on improving the website, please feel free to explore the project on GitHub. Your input and ideas are always welcome!
+                I want to note that the categorizations you see here are entirely my own work, that these do not come from a professional background. If you have suggestions on improving the website, please feel free to explore the project on GitHub. Your input and ideas are highly appreciated!
+                    <br />
+                    <br />
+                Last Updated: Aug 31, 2023
                 </p>
+                   
             </div>
 		</div>
   	);
