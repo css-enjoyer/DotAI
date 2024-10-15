46a80d8914cd617ad5aefadb33bbb159614cce9e
Isaac <isaacjdjreyes@gmail.com>
2024-10-15 10:48:44 +0800

    Change contact ordering

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -7,8 +7,8 @@ function Contact() {
                 <h1>Contact</h1>
             </div>
             <div className="body">
-                <a href="https://github.com/css-enjoyer/DotAI" target="_blank" rel="noreferrer">Project GitHub</a>
                 <p>isaacjdjreyes@gmail.com</p>
+                <a href="https://github.com/css-enjoyer/DotAI" target="_blank" rel="noreferrer">Project GitHub</a>
                 <a href="https://css-enjoyer.github.io/portfolio/" target="_blank" rel="noreferrer">My Portfolio</a>
                 <a href="https://github.com/css-enjoyer" target="_blank" rel="noreferrer">My GitHub</a>
             </div>
